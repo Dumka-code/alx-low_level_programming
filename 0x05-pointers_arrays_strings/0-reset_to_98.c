913a66bfcc78bf63861b0b6fd0b04d813d7343e7
Dumka <preciousdpb2000@gmail.com>
2022-04-21 19:57:43 -0700

    README.md

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * Description: this program Write a function that takes a pointer to an
- * int as parameter and updates the value it points to to 98
- * Return (0)
+ *  reset_to_98 - update address function
+ *  @n: integer
+ *
+ *  Return: nothing
  */
 
 void reset_to_98(int *n)
