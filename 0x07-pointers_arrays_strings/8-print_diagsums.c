fd8d7e58d3cce395108a92564b794e74292edc3b
Dumka <preciousdpb2000@gmail.com>
2022-04-27 11:19:24 -0700

    README.md

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,12 +5,12 @@
  * diagonals of a square matrix of integers
  * @a: input pointer.
  * @size: size of the matrix
- * Return: no return.
+ * *Return: no return.
  */
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
-
+	
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
