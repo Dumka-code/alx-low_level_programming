a4a4362a634b2259a31f75140f5e23721db13ad5
Dumka-code <preciousdpb2000@gmail.com>
2022-05-10 02:26:27 +0100

    mmm

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,28 @@
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * _calloc - allocates memory for an array, using malloc, and initializes to 0
- * @nmemb: number of elements of the array
- * @size: size of each element of the array
- * Return: pointer to the allocated memory (SUCCESS) or
- * NULL if @nmemb or @size is 0 (FAILURE) or
- * NULL if insufficient memory was available (FAILURE)
+ * _calloc - concatenate tw strings specially
+ * @nmemb: number of elements
+ * @size: type of elements
+ * Return: nothing
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p = NULL;
 	unsigned int i;
-	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return ('\0');
-
-	ptr = malloc(nmemb * size);
-
-	if (ptr == NULL)
-		return ('\0');
-
-	for (i = 0; i < nmemb * size; i++)
-		ptr[i] = 0;
-
-	return ((void *)ptr);
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+	return (p);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,25 @@
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * array_range - creates an array of integers, ordered from @min to @max
- * @min: first and minimum value of the array to be created
- * @max: last and maximum value of the array to be created
- * Return: pointer to the newly created array (SUCCESS) or
- * NULL if @min > @max (FAILURE) or
- * NULL if insufficient memory was available (FAILURE)
+ * array_range - print from min to max
+ * @min: number of elements
+ * @max: type of elements
+ * Return: nothing
  */
-
 int *array_range(int min, int max)
 {
-	int i, n;
-	int *ptr;
+	int *p = NULL;
+	int i, res = 0;
 
 	if (min > max)
-		return ('\0');
-
-	n = (max - min) + 1;
-	ptr = malloc(sizeof(int) * n);
-
-	if (ptr == NULL)
-		return ('\0');
-
-	for (i = 0; i < n; i++)
-		ptr[i] = min + i;
-
-	return (ptr);
+		return (NULL);
+	res = max - min;
+	p = malloc((res + 1) * sizeof(int));
+	if (p == 0)
+		return (NULL);
+	for (i = 0; i <= res; i++)
+	{
+		p[i] = min + i;
+	}
+	return (p);
 }
